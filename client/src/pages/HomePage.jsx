7fffde7e9d0f6e2ffef7e8982322b028e38efccf
Shubh Shukla <123090996+Shubh0627@users.noreply.github.com>
2025-07-30 12:51:51 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,25 +1,26 @@
-import React, { useContext } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import ProfilePage from './pages/ProfilePage'
-import { Toaster } from "react-hot-toast"
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext, useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
-const App = () => {
 
-  const { authUser } = useContext(AuthContext)
+const HomePage = () => {
+
+  const {selectedUser} = useContext(ChatContext)
 
   return (
-    <div className="bg-[url('./assets/bgImage.svg')] bg-contain">
-      <Toaster/>
-      <Routes>
-        <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login" /> }/>
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/" /> }/>
-        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login" /> }/>
-      </Routes>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+        overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
+          'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}
+        `}>
+            <Sidebar />
+            <ChatContainer />
+            <RightSidebar />
+        </div>
     </div>
   )
 }
 
-export default App
+export default HomePage
